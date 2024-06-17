b9707378070114457858ac123cc3fc86e333383b
motii <108280115+motii8128@users.noreply.github.com>
2024-06-17 14:47:38 +0900

    Add files via upload

--- mcu_fw_esp32.ino
+++ mcu_fw_esp32.ino
@@ -1,135 +1,83 @@
-#include <WiFiUdp.h>
 #include <WiFi.h>
-#include <lwip/dns.h>
+#include <WiFiAP.h>
+#include <WiFiUdp.h>
 
-// Access point settings
-const bool EnableAccessPoint = true;
-const IPAddress wifi_ip(192, 168, 0, 3);
-const IPAddress wifi_gateway(192, 168, 0, 1);
-const IPAddress wifi_subnet(255, 255, 255, 0);
+// USER
+bool enable_ap = true;
+String device_name = "mcu-esp";
+const char* wifi_name = "motiServer";
+const char* wifi_pass = "moti8128";
+const uint8_t local_port = 64205;
+//
 
-// Common settings
-const char *WiFiName = "Test";
-const char *WiFiPass = "Test1000";
-
-String DeviceName = "mcu-esp32-01";
-// END SETTINGS
-
-
-WiFiUDP udp;
-const uint16_t local_port = 64202;
-
-IPAddress default_ip(192, 168, 0, 100);
-IPAddress default_myDns(192, 168, 0, 1);
-IPAddress broadcast_ip(255, 255, 255, 255);
-
-char buf[512];
-
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  //  D2 pin is digital output
-  pinMode(T2, OUTPUT);
-  pinMode(T3, OUTPUT);
-  // D15 pin is pwm output
-  // ledcSetup(0, 12800, 8);
-  // ledcAttachPin(A13, 0);
-
-  // Debug Serial
-  Serial.begin(115200);
-  digitalWrite(LED_BUILTIN, LOW);
-
-  if(EnableAccessPoint)
+// CONSTANTS
+const uint8_t remote_port = 64203;
+const IPAddress wifi_ip(192, 168, 4, 1);
+const IPAddress wifi_gateway(192, 168, 4, 1);
+const IPAddress subnet(255, 255, 255, 0);
+hw_timer_t *timer = NULL;
+bool timer_flag = false;
+void ARDUINO_ISR_ATTR onTimer()
+{
+  if(!timer_flag)
   {
-    if(!WiFi.mode(WIFI_AP))
-    {
-      Serial.println("[ERROR]Failed to setting WiFi mode!!");
-    }
-
-    WiFi.softAPConfig(wifi_ip, wifi_gateway, wifi_subnet);
-
-    Serial.println("[INFO]Start connecting");
-    WiFi.softAP(WiFiName, WiFiPass);
-
-    ip_addr_t dns_server = IPADDR4_INIT_BYTES(8, 8, 8, 8);
-    dns_setserver(0, &dns_server);
-    
-    Serial.println("[INFO]Connection Established!!");
-    if(udp.begin(default_ip, local_port) == 0)
-    {
-      Serial.println("[ERROR] Failed to begin UDP");
-    }
-
-    Serial.printf("[INFO] IP:%s\n", WiFi.softAPIP().toString());
-
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
-  else
-  {
-    if(!WiFi.mode(WIFI_STA))
-    {
-      Serial.println("[ERROR]Failed to setting WiFi mode!!");
-    }
-
-    Serial.println("[INFO]Start connecting");
-    if(WiFi.begin(WiFiName, WiFiPass) != WL_CONNECTED)
-    {
-      Serial.println("[ERROR] Failed to connect WiFi.");
-    }
-
-    Serial.println("[INFO]Connection Established!!");
-    Serial.print("[INFO]");
-    Serial.println(WiFi.SSID());
-    udp.begin(local_port);
-
-    digitalWrite(LED_BUILTIN, HIGH);
+    timer_flag = true;
   }
 }
 
+WiFiUDP udp;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  delay(1000);
+
+  if(enable_ap)
+  {
+    WiFi.softAPConfig(wifi_ip, wifi_gateway, subnet);
+    if (!WiFi.softAP(wifi_name, wifi_pass, 2, 0, 4, false)) {
+      Serial.println("Soft AP creation failed.");
+      while (1);
+    }
+  }
+  else
+  {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(wifi_name, wifi_pass);
+    if(WiFi.status() != WL_CONNECTED)
+    {
+      Serial.println("Failed to connect to WiFi.");
+      // while(1);
+    }
+  }
+  udp.begin(local_port);
+  Serial.printf("Start WiFi. name:%s, ip_addr:%s", WiFi.softAPSSID(), WiFi.softAPIP().toString());
+
+  timer = timerBegin(1000000);
+  timerAttachInterrupt(timer, &onTimer);
+  timerAlarm(timer, 1000000, true, 0);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
 void loop() {
-  int packet_size = udp.parsePacket();
-  if(packet_size > 0){
-    for(auto i = 0; i < 512; i++)
+  if(timer_flag)
+  {
+    String send_msg = WiFi.softAPIP().toString() + "," + device_name;
+    int err = udp.beginPacket(WiFi.softAPBroadcastIP(), remote_port);
+    if(err != 1)
     {
-      buf[i] = 0;
+      Serial.println("Failed to send begin packet");
+    }
+    udp.printf(send_msg.c_str());
+    err = udp.endPacket();
+    if(err != 1)
+    {
+      Serial.println("Failed to send end packet");
     }
 
-    udp.read(buf, 512);
-
-    String string_data(buf);
-    string_data.trim();
-
-    int power_int = string_data.toInt();
-
-    if(string_data != "0" && power_int == 0)
-    {
-      Serial.println("Unknow data");
-      return;
-    }
-
-    if(power_int > 0)
-    {
-      digitalWrite(T2, HIGH);
-    }
-    else
-    {
-      digitalWrite(T2, LOW);
-    }
-
-    uint pwm_rate = abs(power_int);
-    pwm_rate = (int)((float)(pwm_rate)*2.55);
-
-    if(pwm_rate > 255)
-    {
-      pwm_rate = 255;
-    }
-
-    // ledcWrite(0, pwm_rate);
-    dacWrite(T3, pwm_rate);
-
-    String send_str = WiFi.localIP().toString() + "," + DeviceName;
-    Serial.println(send_str.c_str());
-    udp.beginPacket(broadcast_ip, 64203);
-    udp.printf(send_str.c_str());
-    udp.endPacket();
+    timer_flag = false;
   }
 }
